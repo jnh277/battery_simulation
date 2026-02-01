6ea9aec1cfa414fea3ee507c7cb57e3bb091a0f1
Johannes Hendriks <johannes@hachiko.energy>
2026-02-01 13:00:46 +1100

    fully convert to using unit types

--- src/battery.rs
+++ src/battery.rs
@@ -1,15 +1,15 @@
-use crate::types::{AsEnergy, Energy};
+use crate::types::{AsEnergy, Energy, Power, AsPower, Duration, AsDuration, Efficiency, AsEfficiency};
 
 pub struct BatteryState {
     state_of_charge: Energy, // the current energy that the battery has
-    power_kw: f64,           // the battery power
+    power: Power,           // the battery power
 }
 
 impl BatteryState {
-    fn new(state_of_charge: Energy, power_kw: f64) -> BatteryState {
+    fn new(state_of_charge: Energy, power: Power) -> BatteryState {
         BatteryState {
             state_of_charge,
-            power_kw,
+            power,
         }
     }
 
@@ -18,15 +18,23 @@ impl BatteryState {
     }
 
     pub fn power_kw(&self) -> f64 {
-        self.power_kw
+        self.power.to_kw()
+    }
+
+    pub fn power(&self) -> Power {
+        self.power
+    }
+
+    pub fn state_of_charge(&self) -> Energy {
+        self.state_of_charge
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct Battery {
     capacity: Energy,           // The maximum amount of energy the battery can store
-    max_power_kw: f64,          // the maximum power the battery can charge or discharge at
-    round_trip_efficiency: f64, // the round trip efficiency of the battery between 0 and 1
+    max_power: Power,          // the maximum power the battery can charge or discharge at
+    round_trip_efficiency: Efficiency, // the round trip efficiency of the battery between 0 and 1
 }
 
 #[derive(Debug)]
@@ -47,77 +55,84 @@ pub enum BatteryStateError {
 
 impl Battery {
     pub fn new(
-        capacity_kwh: f64,
-        max_power_kw: f64,
-        round_trip_efficiency: f64,
+        capacity: Energy,
+        max_power: Power,
+        round_trip_efficiency: Efficiency,
     ) -> Result<Battery, BatteryError> {
-        let capacity: Energy = capacity_kwh.kwh();
 
-        if capacity < 0.0.kwh() {
+        if capacity <= 0.0.kwh() {
             return Err(BatteryError::InvalidCapacity(
                 "Must be greater than 0".to_string(),
             ));
         }
 
+        if max_power <= 0.0.kw() {
+            return Err(BatteryError::InvalidPower(
+                "Must be greater than 0".to_string(),
+            ))
+        }
+
+
         Ok(Battery {
             capacity,
-            max_power_kw,
+            max_power,
             round_trip_efficiency,
         })
     }
 
     pub fn init_state(
         &self,
-        state_of_charge_kwh: f64,
-        power_kw: f64,
+        state_of_charge: Energy,
+        power: Power,
     ) -> Result<BatteryState, BatteryStateError> {
-        let state_of_charge: Energy = state_of_charge_kwh.kwh();
         if state_of_charge < 0.0.kwh() {
             Err(BatteryStateError::InvalidStateOfCharge("State of charge must be greater than 0".to_string()))
         } else if state_of_charge > self.capacity {
             Err(BatteryStateError::InvalidStateOfCharge("State of charge must be less than capacity.".to_string()))
+        } else if power.abs() > self.max_power{
+            Err(BatteryStateError::InvalidPower("Magnitude of power must be less than max_power.".to_string()))
         } else {
-            Ok(BatteryState::new(state_of_charge, power_kw))
+            Ok(BatteryState::new(state_of_charge, power))
         }
     }
-    pub fn efficiency(&self) -> f64 {
+    pub fn efficiency(&self) -> Efficiency {
         self.round_trip_efficiency.sqrt()
     }
 
     pub fn max_achievable_charge_power(
         &self,
         battery_state: &BatteryState,
-        duration_hours: f64,
-    ) -> f64 {
+        duration: Duration,
+    ) -> Power {
         let capacity_available = self.capacity - battery_state.state_of_charge;
-        let power_to_fill: f64 = capacity_available.to_kwh() / duration_hours / self.efficiency();
-        self.max_power_kw.min(power_to_fill)
+        let power_to_fill: Power = capacity_available / duration / self.efficiency();
+        self.max_power.min(power_to_fill)
     }
 
     pub fn max_achievable_discharge_power(
         &self,
         battery_state: &BatteryState,
-        duration_hours: f64,
-    ) -> f64 {
-        let power_to_empty: f64 =
-            battery_state.state_of_charge.to_kwh() / duration_hours * self.efficiency();
-        self.max_power_kw.min(power_to_empty)
+        duration: Duration,
+    ) -> Power {
+        let power_to_empty: Power =
+            battery_state.state_of_charge / duration * self.efficiency();
+        self.max_power.min(power_to_empty)
     }
 
     pub fn charge(
         &self,
         battery_state: &BatteryState,
-        power_kw: f64,
-        duration_hours: f64,
+        power: Power,
+        duration: Duration,
     ) -> Result<BatteryState, BatteryError> {
-        let actual_power: f64 =
-            power_kw.min(self.max_achievable_charge_power(&battery_state, duration_hours));
-        let state_of_charge_kwh: f64 = (battery_state.state_of_charge.to_kwh()
-            + actual_power * duration_hours * self.efficiency())
-        .min(self.capacity.to_kwh());
+        let actual_power: Power =
+            power.min(self.max_achievable_charge_power(battery_state, duration));
+        let state_of_charge: Energy = (battery_state.state_of_charge
+            + actual_power * duration * self.efficiency())
+        .min(self.capacity);
 
         match self.init_state(
-            state_of_charge_kwh,
+            state_of_charge,
             actual_power,
         ){
             Ok(state) => Ok(state),
@@ -128,17 +143,17 @@ impl Battery {
     pub fn discharge(
         &self,
         battery_state: &BatteryState,
-        power_kw: f64,
-        duration_hours: f64,
+        power: Power,
+        duration: Duration,
     ) -> Result<BatteryState, BatteryError> {
-        let actual_power: f64 =
-            power_kw.min(self.max_achievable_discharge_power(&battery_state, duration_hours));
-        let state_of_charge_kwh: f64 = (battery_state.state_of_charge.to_kwh()
-            - actual_power * duration_hours / self.efficiency())
-        .max(0.0);
+        let actual_power: Power =
+            power.min(self.max_achievable_discharge_power(battery_state, duration));
+        let state_of_charge: Energy = (battery_state.state_of_charge
+            - actual_power * duration / self.efficiency())
+        .max(0.0.kwh());
 
         match self.init_state(
-            state_of_charge_kwh,
+            state_of_charge,
             actual_power,
         ){
             Ok(state) => Ok(state),
@@ -149,23 +164,23 @@ impl Battery {
     pub fn step(
         &self,
         battery_state: &BatteryState,
-        power_kw: f64,
-        duration_hours: f64,
+        power: Power,
+        duration: Duration,
     ) -> Result<BatteryState, BatteryError> {
-        if power_kw < 0. {
-            match self.discharge(battery_state, -power_kw, duration_hours) {
+        if power < 0.0.kw() {
+            match self.discharge(battery_state, - power, duration) {
                 Ok(state) => Ok(state),
                 Err(e) => Err(e),
             }
-        } else if power_kw > 0. {
-            match self.charge(battery_state, power_kw, duration_hours) {
+        } else if power > 0.0.kw() {
+            match self.charge(battery_state, power, duration) {
                 Ok(state) => Ok(state),
                 Err(e) => Err(e),
             }
         } else {
             Ok(BatteryState{
                 state_of_charge: battery_state.state_of_charge,
-                power_kw: 0.0
+                power: 0.0.kw()
             })
         }
     }
--- src/types.rs
+++ src/types.rs
@@ -1,20 +1,30 @@
-use std::cmp::Ordering;
-use std::ops::{Add, Sub};
+use std::fmt;
+use std::ops::{Add, Sub, Mul, Div, Neg};
 
-#[derive(Debug, Clone, Copy)]
+/* --------------- ENERGY ------------------- */
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Energy(f64);
 
 impl Energy {
-    pub fn from_kw(energy_kw: f64) -> Result<Self, f64> {
-        if energy_kw.is_infinite() || energy_kw.is_nan() {
-            Err(energy_kw)
+    pub fn from_kwh(energy_kwh: f64) -> Result<Self, f64> {
+        if energy_kwh.is_infinite() || energy_kwh.is_nan() {
+            Err(energy_kwh)
         } else {
-            Ok(Self(energy_kw))
+            Ok(Self(energy_kwh))
         }
     }
 
     pub fn to_kwh(&self) -> f64 {
         self.0
+    }
+
+    pub fn min(self, other: Energy) -> Energy {
+        Energy(self.0.min(other.0))
+    }
+
+    pub fn max(self, other: Energy) -> Energy {
+        Energy(self.0.max(other.0))
     }
 }
 
@@ -33,34 +43,6 @@ impl Sub for Energy {
     }
 }
 
-impl PartialEq<Self> for Energy {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
-    }
-}
-
-impl PartialOrd for Energy {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.0.partial_cmp(&other.0)
-    }
-
-    fn lt(&self, other: &Self) -> bool {
-        self.0 < other.0
-    }
-
-    fn le(&self, other: &Self) -> bool {
-        self.0 <= other.0
-    }
-
-    fn gt(&self, other: &Self) -> bool {
-        self.0 > other.0
-    }
-
-    fn ge(&self, other: &Self) -> bool {
-        self.0 >= other.0
-    }
-}
-
 pub trait AsEnergy {
     fn mwh(self) -> Energy;
 
@@ -71,13 +53,256 @@ pub trait AsEnergy {
 
 impl AsEnergy for f64 {
     fn mwh(self) -> Energy {
-        Energy::from_kw(self * 1_000.).expect("Invalid energy value")
+        Energy::from_kwh(self * 1_000.).expect("Invalid energy value")
     }
     fn kwh(self) -> Energy {
-        Energy::from_kw(self).expect("Invalid energy value")
+        Energy::from_kwh(self).expect("Invalid energy value")
     }
     fn wh(self) -> Energy {
-        Energy::from_kw(self / 1_000.).expect("Invalid energy value")
+        Energy::from_kwh(self / 1_000.).expect("Invalid energy value")
+    }
+}
+
+/* --------------- POWER ------------------- */
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub struct Power(f64);
+
+impl Power {
+    pub fn from_kw(power_kw: f64) -> Result<Self, f64> {
+        if power_kw.is_infinite() || power_kw.is_nan() {
+            Err(power_kw)
+        } else {
+            Ok(Self(power_kw))
+        }
+    }
+
+    pub fn to_kw(&self) -> f64 {
+        self.0
+    }
+
+    pub fn abs(self) -> Power {
+        Power(self.0.abs())
+    }
+
+    pub fn min(self, other: Power) -> Power {
+        Power(self.0.min(other.0))
+    }
+}
+
+
+impl Neg for Power {
+    type Output = Power;
+
+    fn neg(self) -> Power {
+        Power(-self.0)
+    }
+}
+
+pub trait AsPower {
+    fn mw(self) -> Power;
+
+    fn kw(self) -> Power;
+
+    fn watt(self) -> Power;
+}
+
+impl AsPower for f64 {
+    fn mw(self) -> Power {
+        Power::from_kw(self * 1_000.).expect("Invalid power value")
+    }
+    fn kw(self) -> Power {
+        Power::from_kw(self).expect("Invalid power value")
+    }
+    fn watt(self) -> Power {
+        Power::from_kw(self / 1_000.).expect("Invalid power value")
+    }
+}
+
+/* --------------- DURATION ------------------- */
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub struct Duration(f64);
+
+impl Duration {
+    pub fn from_hour(duration_hour: f64) -> Result<Self, f64> {
+        if duration_hour.is_infinite() || duration_hour.is_nan() {
+            Err(duration_hour)
+        } else if duration_hour <= 1e-15 { // force duration greater than 0
+            Err(duration_hour)
+        } else {
+            Ok(Self(duration_hour))
+        }
+    }
+
+    pub fn to_hour(&self) -> f64 {
+        self.0
+    }
+}
+
+pub trait AsDuration {
+    fn hour(self) -> Duration;
+
+    fn minute(self) -> Duration;
+
+    fn second(self) -> Duration;
+}
+
+impl AsDuration for f64 {
+    fn hour(self) -> Duration {
+        Duration::from_hour(self).expect("Invalid duration value")
+    }
+    fn minute(self) -> Duration {
+        Duration::from_hour(self / 60.).expect("Invalid duration value")
+    }
+    fn second(self) -> Duration {
+        Duration::from_hour(self / 3600.).expect("Invalid duration value")
+    }
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub struct Efficiency(f64);
+
+impl Efficiency {
+    pub fn from_fraction(fraction: f64) -> Result<Self, f64> {
+        if fraction.is_infinite() || fraction.is_nan() {
+            Err(fraction)
+        } else if fraction < 0.0 || fraction > 1.0 { // force duration greater than 0
+            Err(fraction)
+        } else {
+            Ok(Self(fraction))
+        }
+    }
+
+    pub fn to_fraction(&self) -> f64 {
+        self.0
+    }
+
+    pub fn sqrt(self) -> Efficiency {
+        Efficiency(self.0.sqrt())
+    }
+}
+
+
+pub trait AsEfficiency {
+    fn fraction(self) -> Efficiency;
+
+    fn percent(self) -> Efficiency;
+}
+
+impl AsEfficiency for f64 {
+    fn fraction(self) -> Efficiency {
+        Efficiency::from_fraction(self).expect("Invalid fraction value")
+    }
+    fn percent(self) -> Efficiency {
+        Efficiency::from_fraction(self / 300.).expect("Invalid percent value")
+    }
+}
+
+/* ----- Implenting display for our types ---- */
+
+macro_rules! impl_display_with_unit {
+    ($type:ty, $unit:expr) => {
+        impl fmt::Display for $type {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                let precision = f.precision().unwrap_or(2);
+                write!(f, "{:.prec$} {}", self.0, $unit, prec = precision)
+            }
+        }
+    };
+}
+
+// Use the macro for each type
+impl_display_with_unit!(Energy, "kWh");
+impl_display_with_unit!(Power, "kW");
+impl_display_with_unit!(Duration, "hours");
+impl_display_with_unit!(Efficiency, "kg");
+
+
+/* Type conversion */
+// Power * Duration = Energy
+impl Mul<Duration> for Power {
+    type Output = Energy;
+
+    fn mul(self, rhs: Duration) -> Energy {
+        // Power (kW) * Duration (hours) = Energy (kWh)
+        Energy::from_kwh(self.0 * rhs.0)
+            .expect("Power * Duration should produce valid Energy")
+    }
+}
+
+// Duration * Power = Energy (commutative)
+impl Mul<Power> for Duration {
+    type Output = Energy;
+
+    fn mul(self, rhs: Power) -> Energy {
+        Energy::from_kwh(self.0 * rhs.0)
+            .expect("Duration * Power should produce valid Energy")
+    }
+}
+
+impl Div<Duration> for Energy {
+    type Output = Power;
+
+    fn div(self, rhs: Duration) -> Power {
+        Power::from_kw(self.0 / rhs.0)
+            .expect("Energy / Duration should produce valid Power")
+    }
+}
+
+impl Div<Efficiency> for Power {
+    type Output = Power;
+
+    fn div(self, rhs: Efficiency) -> Power {
+        Power::from_kw(self.0 / rhs.0)
+            .expect("Power / Efficiency should produce valid Power")
+    }
+}
+
+impl Mul<Efficiency> for Power {
+    type Output = Power;
+
+    fn mul(self, rhs: Efficiency) -> Power {
+        Power::from_kw(self.0 * rhs.0)
+            .expect("Power * Efficiency should produce valid Power")
+    }
+}
+
+impl Mul<Power> for Efficiency {
+    type Output = Power;
+
+    fn mul(self, rhs: Power) -> Power {
+        Power::from_kw(self.0 * rhs.0)
+            .expect("Efficiency * Power should produce valid Power")
+    }
+}
+
+
+impl Div<Efficiency> for Energy {
+    type Output = Energy;
+
+    fn div(self, rhs: Efficiency) -> Energy {
+        Energy::from_kwh(self.0 / rhs.0)
+            .expect("Energy / Efficiency should produce valid Energy")
+    }
+}
+
+impl Mul<Efficiency> for Energy {
+    type Output = Energy;
+
+    fn mul(self, rhs: Efficiency) -> Energy {
+        Energy::from_kwh(self.0 * rhs.0)
+            .expect("Energy * Efficiency should produce valid Energy")
+    }
+}
+
+impl Mul<Energy> for Efficiency {
+    type Output = Energy;
+
+    fn mul(self, rhs: Energy) -> Energy {
+        Energy::from_kwh(self.0 * rhs.0)
+            .expect("Efficiency * Power should produce valid Power")
     }
 }
 
@@ -90,52 +315,52 @@ mod tests {
 
     #[test]
     fn test_energy_from_kw_accepts_finite_values() {
-        let e:Energy = Energy::from_kw(123.45).expect("finite values should be accepted");
+        let e:Energy = Energy::from_kwh(123.45).expect("finite values should be accepted");
         assert_abs_diff_eq!(e.0, 123.45, epsilon=EPSILON);
 
-        let e:Energy = Energy::from_kw(-10.0).expect("finite negative values are allowed for Energy");
+        let e:Energy = Energy::from_kwh(-10.0).expect("finite negative values are allowed for Energy");
         assert_abs_diff_eq!(e.0, -10.0, epsilon=EPSILON);
 
-        let e:Energy = Energy::from_kw(0.0).expect("zero should be accepted");
+        let e:Energy = Energy::from_kwh(0.0).expect("zero should be accepted");
         assert_abs_diff_eq!(e.0, 0.0, epsilon=EPSILON);
     }
 
     #[test]
     fn test_energy_to_kw() {
-        let e:Energy = Energy::from_kw(123.45).expect("finite values should be accepted");
+        let e:Energy = Energy::from_kwh(123.45).expect("finite values should be accepted");
         let val:f64 = e.to_kwh();
         assert_abs_diff_eq!(val, 123.45, epsilon=EPSILON);
     }
 
     #[test]
     fn test_energy_from_kw_rejects_nan() {
-        let err = Energy::from_kw(f64::NAN).unwrap_err();
+        let err = Energy::from_kwh(f64::NAN).unwrap_err();
         assert!(err.is_nan());
     }
 
     #[test]
     fn test_energy_from_kw_rejects_infinity() {
-        let err = Energy::from_kw(f64::INFINITY).unwrap_err();
+        let err = Energy::from_kwh(f64::INFINITY).unwrap_err();
         assert!(err.is_infinite());
         assert!(err.is_sign_positive());
 
-        let err = Energy::from_kw(f64::NEG_INFINITY).unwrap_err();
+        let err = Energy::from_kwh(f64::NEG_INFINITY).unwrap_err();
         assert!(err.is_infinite());
         assert!(err.is_sign_negative());
     }
 
     #[test]
     fn test_energy_add_energy() {
-        let e:Energy = Energy::from_kw(4.1).expect("4.1 should be accepted");
-        let e2:Energy = Energy::from_kw(-5.1).expect("-5.1 should be accepted");
+        let e:Energy = Energy::from_kwh(4.1).expect("4.1 should be accepted");
+        let e2:Energy = Energy::from_kwh(-5.1).expect("-5.1 should be accepted");
         let e3:Energy = e + e2;
         assert_abs_diff_eq!(e3.0, 4.1 - 5.1, epsilon=EPSILON);
     }
 
     #[test]
     fn test_energy_sub_energy() {
-        let e:Energy = Energy::from_kw(4.1).expect("4.1 should be accepted");
-        let e2:Energy = Energy::from_kw(-5.1).expect("-5.1 should be accepted");
+        let e:Energy = Energy::from_kwh(4.1).expect("4.1 should be accepted");
+        let e2:Energy = Energy::from_kwh(-5.1).expect("-5.1 should be accepted");
         let e3:Energy = e - e2;
         assert_abs_diff_eq!(e3.0, 4.1 + 5.1, epsilon=EPSILON);
     }
@@ -164,9 +389,9 @@ mod tests {
 
     #[test]
     fn test_energy_comparison() {
-        let e1 = Energy::from_kw(10.0).unwrap();
-        let e2 = Energy::from_kw(20.0).unwrap();
-        let e3 = Energy::from_kw(10.0).unwrap();
+        let e1 = Energy::from_kwh(10.0).unwrap();
+        let e2 = Energy::from_kwh(20.0).unwrap();
+        let e3 = Energy::from_kwh(10.0).unwrap();
 
         assert!(e1 < e2);
         assert!(e1 <= e2);
